5dd1297835479f5ab58c8f9073aec73ebb1dc1b9
Coding4usall <112630854+Coding4usall@users.noreply.github.com>
2022-11-18 22:25:48 -0300

    [JSE]0011 FEATURES/ Add error messages

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -12,17 +12,17 @@ for (var i = 0; i < pacientes.length; i++) {
 
   var tdImc = paciente.querySelector(".info-imc");
 
-  var pesoEhValido = true;
-  var alturaEhValida = true;
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoEhValido) {
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido";
     paciente.classList.add("paciente-invalido");
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!alturaEhValida) {
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
@@ -35,8 +35,25 @@ for (var i = 0; i < pacientes.length; i++) {
   }
 }
 
+function validaPeso(peso) {
+    if (peso < 0 || peso >= 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
+function validaAltura (altura) {
+    if (altura < 0 || altura >= 3.0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 function calculaImc(peso,altura) {
   var imc = 0;
   imc = peso/(altura*altura);
   return imc.toFixed(2);
 }
+
--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -1,54 +1,109 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    var form = document.querySelector("#form-adiciona");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    var pacienteAdicionado = obtemPacienteDoFormulario(form);
+  var form = document.querySelector("#form-adiciona");
+  var pacienteAdicionado = obtemPacienteDoFormulario(form);
 
+  var erros = validaPaciente(pacienteAdicionado);
+    console.log(erros);
+
+  if (erros.length > 0) {
+    exibeMensagemDeErro(erros);
+  } else {
     var pacienteAdicionadoTR = montaTR(pacienteAdicionado);
-
     var tabela = document.querySelector("#tabela-pacientes");
-
     tabela.appendChild(pacienteAdicionadoTR);
-
+    form.reset();
+    document.querySelector('#mensagens-erro').innerHTML = "";
+  }
 });
 
-function obtemPacienteDoFormulario(form){
+function obtemPacienteDoFormulario(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value),
+  };
+
+  return paciente;
+}
+
+function montaTR(pacienteAdicionado) {
+  var pacienteAdicionadoTr = document.createElement("tr");
+  addClass(pacienteAdicionadoTr, "paciente");
+
+  pacienteAdicionadoTr.appendChild(
+    montaTd(pacienteAdicionado.nome, "info-nome")
+  );
+  pacienteAdicionadoTr.appendChild(
+    montaTd(pacienteAdicionado.peso, "info-peso")
+  );
+  pacienteAdicionadoTr.appendChild(
+    montaTd(pacienteAdicionado.altura, "info-altura")
+  );
+  pacienteAdicionadoTr.appendChild(
+    montaTd(pacienteAdicionado.gordura, "info-gordura")
+  );
+  pacienteAdicionadoTr.appendChild(montaTd(pacienteAdicionado.imc, "info-imc"));
+  console.log(pacienteAdicionadoTr);
+  return pacienteAdicionadoTr;
+}
+
+function addClass(dado, classe) {
+  var tagClass = dado.classList.add(classe);
+  return tagClass;
+}
+
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  addClass(td, classe);
+  return td;
+  s;
+}
+function validaPaciente(paciente) {
+  var erros = [];
+
+  if (paciente.nome.length == 0) {
+    erros.push("Favor Preencher o nome");
+  }
+  if (paciente.peso.length == 0) {
+    erros.push("Favor Preencher o peso");
+  }
+  if (paciente.altura.length == 0) {
+    erros.push("Favor Preencher o altura");
+  }
+
+  if (!validaPeso(paciente.peso)) {
+    erros.push("Peso iválido");
+  }
+
+  if (!validaAltura(paciente.altura)) {
+    erros.push("Altura iválida");
+  }
+
+  if (paciente.gordura.length == 0) {
+    erros.push("Favor Preencher o % de gordura");
+  }
+
+  return erros;
+}
+
+function exibeMensagemDeErro(erros) {
     
-    var paciente = {
-        nome: form.nome.value,
-        peso: form.peso.value,
-        altura: form.altura.value,
-        gordura: form.gordura.value,
-        imc: calculaImc(form.peso.value,form.altura.value)
-    }
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
 
-    return paciente;
-}
-
-function montaTR(pacienteAdicionado){
-    var pacienteAdicionadoTr = document.createElement("tr");
-    pacienteAdicionadoTr.classList.add("paciente");
-
-    var nomeTd = montaTd(pacienteAdicionado.nome,'info-nome')
-    var pesoTd = montaTd(pacienteAdicionado.peso,'info-peso')
-    var alturaTd = montaTd(pacienteAdicionado.altura,'info-altura')
-    var gorduraTd = montaTd(pacienteAdicionado.gordura,'info-gordura')
-    var imcTd = montaTd(pacienteAdicionado.imc,'info-imc')
-
-    pacienteAdicionadoTr.appendChild(nomeTd);
-    pacienteAdicionadoTr.appendChild(pesoTd);
-    pacienteAdicionadoTr.appendChild(alturaTd);
-    pacienteAdicionadoTr.appendChild(gorduraTd);
-    pacienteAdicionadoTr.appendChild(imcTd);
-
-    return pacienteAdicionadoTr;
-}
-
-function montaTd(dado,classe) {
-    var td = document.createElement("td");
-    td.textContent = dado;
-    td.classList.add(classe);
-    return td;
+    erros.forEach(function(erro){
+        var li = document.createElement('li');
+        li.textContent = erro;
+        li.classList.add('erro');
+        ul.appendChild(li);
+    })   
+    
+    console.log(ul);
 }
